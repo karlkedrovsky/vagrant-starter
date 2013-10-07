3da692507855d802d5cd54cc42c05d2dad600a5b
Karl Kedrovsky <karl@kedrovsky.com>
2013-10-07 16:11:17 -0500

    Updated personal provisioning to run commands as the vagrant user.

--- provision_personal.sh
+++ provision_personal.sh
@@ -1,13 +1,13 @@
 echo "[vagrant provisioning] Setting git config variables..."
-git config --global user.name "Karl Kedrovsky"
-git config --global user.email karl@kedrovsky.com
+sudo -u vagrant git config --global user.name "Karl Kedrovsky"
+sudo -u vagrant git config --global user.email karl@kedrovsky.com
 
 echo "[vagrant provisioning] Setting up custom config files..."
-git clone -q https://github.com/karlkedrovsky/oh-my-zsh.git /home/vagrant/oh-my-zsh
-git clone -q https://github.com/karlkedrovsky/config.git /home/vagrant/config
-ln -s /home/vagrant/config/zshrc /home/vagrant/.zshrc
-ln -s /home/vagrant/config/aliases /home/vagrant/.aliases
-ln -s /home/vagrant/config/drush /home/vagrant/.drush
+sudo -u vagrant git clone -q https://github.com/karlkedrovsky/oh-my-zsh.git /home/vagrant/oh-my-zsh
+sudo -u vagrant git clone -q https://github.com/karlkedrovsky/config.git /home/vagrant/config
+sudo -u vagrant ln -s /home/vagrant/config/zshrc /home/vagrant/.zshrc
+sudo -u vagrant ln -s /home/vagrant/config/aliases /home/vagrant/.aliases
+sudo -u vagrant ln -s /home/vagrant/config/drush /home/vagrant/.drush
 
 echo "[vagrant provisioning] Setting vagrant shell to zsh..."
 chsh -s /usr/bin/zsh vagrant
